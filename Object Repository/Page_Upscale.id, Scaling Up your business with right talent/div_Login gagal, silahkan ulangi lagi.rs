9f2a2e08d197f19d50c36263ab7b3f25f51fa096
asyiddiki <hasbi3108@gmail.com>
2023-07-25 18:11:13 +0700

    Katalon Web Testing

---
+++ Object Repository/Page_Upscale.id, Scaling Up your business with right talent/div_Login gagal, silahkan ulangi lagi.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Login gagal, silahkan ulangi lagi</name>
+   <tag></tag>
+   <elementGuidId>36fc5e0c-3141-4231-8649-37fc68505f43</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//form[@id='login-form']/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.info.alert.alert-warning</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>1d61033a-73f8-478e-bd7f-1a4307c1adea</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>info alert alert-warning</value>
+      <webElementGuid>6385f12b-07cf-4513-a6ad-43eb2c5b94da</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Login gagal, silahkan ulangi lagi</value>
+      <webElementGuid>b1f2edda-a298-4cd6-91e8-47e8d4ff677f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;login-form&quot;)/div[@class=&quot;info alert alert-warning&quot;]</value>
+      <webElementGuid>e59f7623-f042-4675-9218-c8d67530b828</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='login-form']/div</value>
+      <webElementGuid>759797d6-d0c7-4852-b4cf-37876d9619a3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login Dashboard'])[1]/following::div[1]</value>
+      <webElementGuid>6d82758e-830b-4b8e-ac63-5b2a3da1bbb0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Email'])[1]/preceding::div[1]</value>
+      <webElementGuid>652be757-df1e-4c99-997d-b5ece90af2d0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/preceding::div[5]</value>
+      <webElementGuid>1f6c2748-61b8-4f91-8c02-fa0ececb9ec6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Login gagal, silahkan ulangi lagi']/parent::*</value>
+      <webElementGuid>7dac3fdd-097a-459f-abb8-ea0084a950f7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/div</value>
+      <webElementGuid>9dcc0c3e-634f-4738-83af-6c83ea1202cd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Login gagal, silahkan ulangi lagi' or . = 'Login gagal, silahkan ulangi lagi')]</value>
+      <webElementGuid>98d29612-69cd-4bca-a16b-7d66a1eb785f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
